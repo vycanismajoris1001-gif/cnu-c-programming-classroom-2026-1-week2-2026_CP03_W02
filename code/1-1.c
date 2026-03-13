f7acc9d40bfb67dbc3df1727f602cf5dc3b5d2b5
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-13 09:30:08 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+   int var1 = 100;
+    printf("int : %d\n", var1);
+    printf("hex : %x\n", var1);
+    printf("oct : %o\n", var1);
 
+    char var2 = 'c';
+    printf("char : %c\n", var2);
+
+    char *var3 = "hello world";
+    printf("string : %s\n", var3);
+
+    float var4 = 3.141592;
+    printf("float: %f\n", var4);
+    printf("exp : %e\n", var4);
     return 0;
 }
