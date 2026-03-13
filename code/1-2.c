4846f281b0aef69f77dcfa8e5be1c976c71b035c
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-03-13 09:37:35 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main()
 {
-
+    
+    printf("%s %s %s\n", "name", "id", "value");
+    printf("%s %10d %.2f\n", "kim", 12, 4.12);
+    printf("%s %10d %.2f\n", "lee", 1922, 3.21);
+    printf("%s %10d %.2f\n", "park", 432, 1.0);
 
     return 0;
+    
 }
